f0fb457c8553a3a39a4208a6aa7a0df14de86431
Lukas Magel <luke-mail@online.de>
2017-12-20 21:13:08 +0100

    Refactored the crawler

--- config.py
+++ config.py
@@ -1,30 +1,26 @@
 import re
+from types import SimpleNamespace
 
+worker_count = 1
 access_token = "***REMOVED***"
 
-worker_count = 8
-
-
-# Search all repositories with:
-# - 75 to 150 stars
-# - updated in the last 12 months
-# - smaller than 10 MB TODO remove?
-github_repo_query = 'stars:75..150 pushed:>2017-01-08 size:<=10000'
+# Search parameters
+# github_repo_query = 'stars:75..150 pushed:>2017-01-08 size:<=10000'
+search_params = {
+    "stars": (75, 150),
+    "last_accessed": "2017-01-08",
+    "max_size": 10000,
+}
 
 progress_file = "processed_repos.txt"
 
-file_types = {
-    "sql_injection":    ("php"),
-    "xss":              ("php"),
-    "buffer_overflow":  ("c", "cpp")
+sql_config = {
+    "name": "php-sqlinj",
+    "language": "php",
+    "filetypes": ("php"),
+    "regex": re.compile(b"(\"\s*(SELECT|INSERT|DELETE).*?{?\$[a-zA-Z_0-9]+}?.*?\")", re.IGNORECASE | re.MULTILINE),
 }
 
-__sql_query = re.compile(b"(.*(SELECT|INSERT).* \$_GET\[.*\])", re.DOTALL | re.IGNORECASE | re.MULTILINE)
-__xss_query = re.compile(b"(.*\$_GET\[.*\])", re.DOTALL | re.IGNORECASE | re.MULTILINE)
-__bo_query = re.compile(b"(.*(strcpy).*)", re.DOTALL | re.IGNORECASE | re.MULTILINE)
-
-regex_queries = {
-    "sql_injection":    __sql_query,
-    "xss":              __xss_query,
-    "buffer_overflow":  __bo_query
-}
+configs = {c["name"]: SimpleNamespace(**c) for c in (
+    sql_config,
+)}
--- crawler.py
+++ crawler.py
@@ -5,7 +5,8 @@
 
 # IMPORTS
 import os
-import re
+import sys
+import time
 import mmap
 from concurrent.futures import ThreadPoolExecutor
 
@@ -19,55 +20,94 @@ import config
 def check_data(data, pattern):
     # Checks if the given data contains a match for the given query
     matches = pattern.findall(data)
+
+    # As result set we return the content of the first group for each matched regular expression
+    result = []
     for match in matches:
-        print(match)
+        if match is str:
+            result.append(match)
+        else:
+            result.append(match[0])
+    return result
 
-
-def check_file(file):
+def check_file(file, search_pattern):
     # Check if the given file contains any matches for one of our vulnerabilities
     with open(file=file, mode="r", encoding="UTF-8") as f:
-        data = mmap.mmap(fileno=f.fileno(), length=0, prot=mmap.PROT_READ)
-        if file.endswith(config.file_types["sql_injection"]):
-            check_data(data, config.regex_queries["sql_injection"])
-        if file.endswith(config.file_types["xss"]):
-            check_data(data, config.regex_queries["xss"])
-        if file.endswith(config.file_types["buffer_overflow"]):
-            check_data(data, config.regex_queries["buffer_overflow"])
+        map = mmap.mmap(fileno=f.fileno(), length=0, prot=mmap.PROT_READ)
+        return check_data(map, search_pattern)
 
-def check_folder(folder):
+def search_folder(folder, filetypes, search_pattern):
     # Check if the given folder contains any files with matches for one of our vunerabilities
-    for dname, dirs, files in os.walk(folder):
+    results = []
+    for dname, _, files in os.walk(folder):
         for fname in files:
             fpath = os.path.join(dname, fname)
-            if os.stat(fpath).st_size == 0:    # Empty file
-                continue
-            check_file(fpath)
+            if os.stat(fpath).st_size > 0 and has_filetype(fpath, filetypes):
+                matches = check_file(fpath, search_pattern)
 
-def check_repo(repo):
+                # Only append files which actually have any matches
+                if len(matches) > 0:
+                    results.append((fname, matches))
+    return results
+
+def has_filetype(fname, filetypes):
+    for ftype in filetypes:
+        if fname.endswith(ftype):
+            return True
+    return False
+
+def check_repository(repo, search_conf):
     try:
-        print("Searching in %s" % repo.full_name)
-        if not os.path.exists("tmp/%s" % repo.full_name):
-            os.makedirs("tmp/%s" % repo.full_name)
-        Git("tmp/%s" % repo.full_name).clone(repo.clone_url, "--depth", "1")
-        check_folder("tmp/%s" % repo.full_name)
-        rmtree("tmp/%s" % repo.full_name)
-        print("Finished %s" % repo.full_name)
+        path = clone_repository("tmp", repo)
+        check_folder(path, search_conf)
+        rmtree(path)
+
     except Exception as e:
-        print(e)
+        print("Exception occurred while searching: %s" % e)
 
+def check_folder(path, search_conf):
+    print("Searching in %s" % path)
+    results = search_folder(path, search_conf.filetypes, search_conf.regex)
 
+    if len(results) > 0:
+        for fpath, matches in results:
+            print("Possibly vulnerable file: %s" % fpath)
+            for match in matches:
+                print("\t%s" % match)
 
-# VARIABLES
-g = Github(login_or_token=config.access_token)
-repos = g.search_repositories(query=config.github_repo_query, sort='stars', order='desc')
-i = 0
-with ThreadPoolExecutor(max_workers=config.worker_count) as executer, open(config.progress_file, "r+") as file:
-    lines = tuple(file)
-    lines = map(lambda s: s.strip(), lines) # Remove newline characters
-    for repo in repos:
-        if not repo.full_name in lines:  # TODO fix
-            file.write(repo.full_name + "\n")
-            executer.submit(check_repo, repo)
-            i = i + 1
-            if i == 1:
-                break
+    print("Finished %s" % path)
+
+def clone_repository(rootdir, repo):
+    repo_path = "%s/%s" % (rootdir, repo.full_name)
+    if not os.path.exists(repo_path):
+        os.makedirs(repo_path)
+    Git(repo_path).clone(repo.clone_url, "--depth", "1")
+    return repo_path
+
+def build_query(stars, last_accessed, max_size, language):
+    query = "stars:%i..%i pushed:>%s size:<%i language:%s" % (
+        stars[0], stars[1], last_accessed, max_size, language
+    )
+    return query
+
+def execute_search(repos, search_conf, workers):
+    fn = lambda repo: check_repository(repo, search_conf)
+    with ThreadPoolExecutor(max_workers=workers) as executor:
+        results = executor.map(fn, repos)
+        for result in results:
+            print(result)
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print("Please specify a search config name")
+        sys.exit(1)
+
+    search_conf = config.configs[sys.argv[1]]
+    print("Using configuration: %s" % search_conf.name)
+
+    ghub = Github(login_or_token=config.access_token)
+    query = build_query(language=search_conf.language, **config.search_params)
+    repos = ghub.search_repositories(query=query, sort='stars', order='desc')
+
+    execute_search(repos, search_conf, config.worker_count)
